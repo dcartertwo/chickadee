d764f4914f18b8938d97f2c87c77c9cff782831c
Anton Begehr <anton@begehr.me>
2025-04-14 13:56:16 +0200

    try cors

--- packages/service/src/api/index.ts
+++ packages/service/src/api/index.ts
@@ -12,7 +12,18 @@ app.use(
   cors({
     origin: (origin) => origin, // allow all origins
     allowMethods: ["OPTIONS", "POST"],
-    allowHeaders: ["Content-Type"],
+    allowHeaders: [
+      "Content-Type",
+      "Accept",
+      "Origin",
+      "Referer",
+      "User-Agent",
+      "Sec-Fetch-Dest",
+      "Sec-Fetch-Mode",
+      "Sec-Fetch-Site",
+    ],
+    exposeHeaders: ["Content-Length"],
+    maxAge: 600, // 10 minutes
   })
 );
 
@@ -27,59 +38,64 @@ app.post(
     z.object({ d: z.string(), u: z.string().url(), r: z.string() })
   ),
   async (c) => {
-    const { d: domain, u, r: referrer } = c.req.valid("json");
-    const url = new URL(u);
-    const host = url.host;
-    const pathname = url.pathname;
-    console.debug("DEBUG body", { domain, url, host, pathname, referrer });
+    try {
+      const { d: domain, u, r: referrer } = c.req.valid("json");
+      const url = new URL(u);
+      const host = url.host;
+      const pathname = url.pathname;
+      console.debug("DEBUG body", { domain, url, host, pathname, referrer });
 
-    // HonoRequest: https://hono.dev/docs/api/request
-    const userAgent = c.req.header("User-Agent");
-    // TODO! parse userAgent
-    console.debug("DEBUG HonoRequest", { userAgent });
+      // HonoRequest: https://hono.dev/docs/api/request
+      const userAgent = c.req.header("User-Agent");
+      // TODO! parse userAgent
+      console.debug("DEBUG HonoRequest", { userAgent });
 
-    // CF Request Properties: https://developers.cloudflare.com/workers/runtime-apis/request/#incomingrequestcfproperties
-    const cf = c.req.raw.cf as IncomingRequestCfProperties | undefined;
-    const country = cf?.country;
-    const region = cf?.regionCode;
-    const city = cf?.city;
-    const latitude = cf?.latitude ? Number.parseFloat(cf?.latitude) : null;
-    const longitude = cf?.longitude ? Number.parseFloat(cf?.longitude) : null;
-    const timezone = cf?.timezone;
-    // CF Bot Management: https://developers.cloudflare.com/bots/concepts/bot-score/
-    const isBot =
-      cf &&
-      (cf.botManagement.verifiedBot ||
-        (cf.botManagement.score > 0 && cf.botManagement.score < 30));
-    console.debug("DEBUG cf parsed", {
-      country,
-      region,
-      city,
-      latitude,
-      longitude,
-      timezone,
-      isBot,
-    });
+      // CF Request Properties: https://developers.cloudflare.com/workers/runtime-apis/request/#incomingrequestcfproperties
+      const cf = c.req.raw.cf as IncomingRequestCfProperties | undefined;
+      const country = cf?.country;
+      const region = cf?.regionCode;
+      const city = cf?.city;
+      const latitude = cf?.latitude ? Number.parseFloat(cf?.latitude) : null;
+      const longitude = cf?.longitude ? Number.parseFloat(cf?.longitude) : null;
+      const timezone = cf?.timezone;
+      // CF Bot Management: https://developers.cloudflare.com/bots/concepts/bot-score/
+      const isBot =
+        cf &&
+        (cf.botManagement.verifiedBot ||
+          (cf.botManagement.score > 0 && cf.botManagement.score < 30));
+      console.debug("DEBUG cf parsed", {
+        country,
+        region,
+        city,
+        latitude,
+        longitude,
+        timezone,
+        isBot,
+      });
 
-    // Connection Info: https://hono.dev/docs/helpers/conninfo
-    const info = getConnInfo(c);
-    const ip = info.remote.address;
-    console.debug("DEBUG connInfo", { ip });
+      // Connection Info: https://hono.dev/docs/helpers/conninfo
+      const info = getConnInfo(c);
+      const ip = info.remote.address;
+      console.debug("DEBUG connInfo", { ip });
 
-    // Write event to Analytics Engine.
-    // * Limits: https://developers.cloudflare.com/analytics/analytics-engine/limits/
-    const event: AnalyticsEngineDataPoint = {
-      // max 1 index, 96 bytes
-      indexes: [],
-      // max 20 blobs, total 5120 bytes
-      blobs: [],
-      // max 20 doubles
-      doubles: [],
-    };
-    if (c.env.ENGINE) c.env.ENGINE.writeDataPoint(event);
-    else console.info("EVENT", event);
+      // Write event to Analytics Engine.
+      // * Limits: https://developers.cloudflare.com/analytics/analytics-engine/limits/
+      const event: AnalyticsEngineDataPoint = {
+        // max 1 index, 96 bytes
+        indexes: [],
+        // max 20 blobs, total 5120 bytes
+        blobs: [],
+        // max 20 doubles
+        doubles: [],
+      };
+      if (c.env.ENGINE) c.env.ENGINE.writeDataPoint(event);
+      else console.info("EVENT", event);
 
-    return c.text("ok", 200);
+      return c.text("ok", 200);
+    } catch (err) {
+      console.error(err);
+      return c.text("Internal Server Error", 500);
+    }
   }
 );
 
