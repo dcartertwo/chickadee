f73c80500b5598b641b43873c625f1f70159033c
Anton Begehr <anton@begehr.me>
2025-04-21 13:26:46 +0200

    clean ya

---
+++ packages/service/app/routes/[sid].tsx
@@ -0,0 +1,21 @@
+import { createRoute } from "honox/factory";
+import { Footer, Header } from "../components/common";
+import Timeline from "../islands/timeline";
+import Menu from "../components/menu";
+import Stats from "../components/stats";
+
+export default createRoute((c) => {
+  return c.render(
+    <div class="h-dvh flex flex-col">
+      <Header />
+
+      <main class="flex-grow flex flex-col p-4 lg:p-8">
+        <Menu />
+        <Stats />
+        <Timeline />
+      </main>
+
+      <Footer />
+    </div>,
+  );
+});
--- packages/service/app/routes/index.tsx
+++ packages/service/app/routes/index.tsx
@@ -1,23 +1,5 @@
 import { createRoute } from "honox/factory";
-import { Footer, Header } from "../components/common";
-import Timeline from "../islands/timeline";
-import Menu from "../components/menu";
-import Stats from "../components/stats";
 
 export default createRoute((c) => {
-  return c.render(
-    <div class="h-dvh flex flex-col">
-      <Header />
-
-      <main class="flex-grow flex flex-col p-4 lg:p-8">
-        <Menu />
-
-        <Stats />
-
-        <Timeline />
-      </main>
-
-      <Footer />
-    </div>,
-  );
+  return c.redirect("/chickadee.me"); // TODO! redirect to default sid
 });
