dcab45ed17c0a380a75f14cfbae4d5ac513fce08
Anton Begehr <anton@begehr.me>
2025-04-21 10:45:09 +0200

    chart component

---
+++ packages/service/app/components/chart.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useRef, useState, type FC } from "hono/jsx";
+import { twMerge } from "tailwind-merge";
+
+export type ChartOptions = Omit<ApexCharts.ApexOptions, "series">;
+export type ChartData = ApexAxisChartSeries | ApexNonAxisChartSeries;
+
+const Chart: FC<{
+  class: string;
+  options: ChartOptions;
+  data: ChartData;
+}> = ({ class: cn, options, data }) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [chart, setChart] = useState<ApexCharts | null>(null);
+
+  // init
+  useEffect(() => {
+    async function init() {
+      const ApexCharts = (await import("apexcharts")).default;
+      if (ref.current) {
+        const chart = new ApexCharts(ref.current, { ...options, series: data });
+        setChart(chart);
+        await chart.render();
+      }
+    }
+    init().catch(console.error);
+  }, []);
+
+  // update
+  useEffect(() => {
+    chart?.updateOptions(options);
+  }, [options]);
+  useEffect(() => {
+    chart?.updateSeries(data);
+  }, [data]);
+
+  return <div ref={ref} class={twMerge(cn)} />;
+};
+
+export default Chart;
--- packages/service/app/islands/timeline.tsx
+++ packages/service/app/islands/timeline.tsx
@@ -1,97 +1,84 @@
-import { useEffect, useRef, type FC } from "hono/jsx";
+import type { FC } from "hono/jsx";
+import Chart, { type ChartData, type ChartOptions } from "../components/chart";
 
 const Timeline: FC = () => {
-  const chartRef = useRef<HTMLDivElement>(null);
+  const options: ChartOptions = {
+    chart: {
+      height: "100%",
+      width: "100%",
+      type: "area",
+      fontFamily: "Inter, sans-serif",
+      dropShadow: {
+        enabled: false,
+      },
+      toolbar: {
+        show: false,
+      },
+    },
+    tooltip: {
+      enabled: true,
+      x: {
+        show: false,
+      },
+    },
+    fill: {
+      type: "gradient",
+      gradient: {
+        opacityFrom: 0.55,
+        opacityTo: 0,
+        shade: "#1C64F2",
+        gradientToColors: ["#1C64F2"],
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      width: 6,
+    },
+    grid: {
+      show: false,
+      strokeDashArray: 4,
+      padding: {
+        left: 2,
+        right: 2,
+        top: 0,
+      },
+    },
+    xaxis: {
+      categories: [
+        "01 February",
+        "02 February",
+        "03 February",
+        "04 February",
+        "05 February",
+        "06 February",
+        "07 February",
+      ],
+      labels: {
+        show: false,
+      },
+      axisBorder: {
+        show: false,
+      },
+      axisTicks: {
+        show: false,
+      },
+    },
+    yaxis: {
+      show: false,
+    },
+  };
 
-  useEffect(() => {
-    const initChart = async () => {
-      const ApexCharts = (await import("apexcharts")).default;
+  const data: ChartData = [
+    {
+      name: "New users",
+      data: [6500, 6418, 6456, 6526, 6356, 6456],
+      color: "#1A56DB",
+    },
+  ];
 
-      const options = {
-        chart: {
-          height: "100%",
-          maxWidth: "100%",
-          type: "area",
-          fontFamily: "Inter, sans-serif",
-          dropShadow: {
-            enabled: false,
-          },
-          toolbar: {
-            show: false,
-          },
-        },
-        tooltip: {
-          enabled: true,
-          x: {
-            show: false,
-          },
-        },
-        fill: {
-          type: "gradient",
-          gradient: {
-            opacityFrom: 0.55,
-            opacityTo: 0,
-            shade: "#1C64F2",
-            gradientToColors: ["#1C64F2"],
-          },
-        },
-        dataLabels: {
-          enabled: false,
-        },
-        stroke: {
-          width: 6,
-        },
-        grid: {
-          show: false,
-          strokeDashArray: 4,
-          padding: {
-            left: 2,
-            right: 2,
-            top: 0,
-          },
-        },
-        series: [
-          {
-            name: "New users",
-            data: [6500, 6418, 6456, 6526, 6356, 6456],
-            color: "#1A56DB",
-          },
-        ],
-        xaxis: {
-          categories: [
-            "01 February",
-            "02 February",
-            "03 February",
-            "04 February",
-            "05 February",
-            "06 February",
-            "07 February",
-          ],
-          labels: {
-            show: false,
-          },
-          axisBorder: {
-            show: false,
-          },
-          axisTicks: {
-            show: false,
-          },
-        },
-        yaxis: {
-          show: false,
-        },
-      };
-
-      if (chartRef.current) {
-        const chart = new ApexCharts(chartRef.current, options);
-        chart.render();
-      }
-    };
-
-    initChart().catch(console.error);
-  }, []);
-
-  return <div ref={chartRef} class="h-96 w-full bg-red-500" />;
+  return <Chart class="h-96 w-full bg-red-500" options={options} data={data} />;
 };
 
 export default Timeline;
