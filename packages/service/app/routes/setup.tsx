05dd4a9293a127b2791b9e34b99cc2b1d6eb6b28
Anton Begehr <anton@begehr.me>
2025-04-23 17:54:12 +0200

    unused imports

--- packages/service/app/routes/setup.tsx
+++ packages/service/app/routes/setup.tsx
@@ -1,5 +1,4 @@
 import { createRoute } from "honox/factory";
-import { Footer, Header } from "../components/common";
 
 export default createRoute(async (c) => {
   return c.render(<h1>TODO! Setup a new site!</h1>);
